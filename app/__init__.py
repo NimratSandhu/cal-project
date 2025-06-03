13c47ceb1809417ae83bee61cb5505154eb8f70d
Nimrat Sandhu <nimratsandhu@Nimrats-MacBook-Pro.local>
2025-06-02 21:06:02 -0400

    Switch to Postgres

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
+
+DB_URL = os.getenv("DB_URL")
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///calendar.db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = DB_URL
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
