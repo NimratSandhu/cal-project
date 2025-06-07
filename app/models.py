4d39cf6aabcfc63182fa1a4a6677da3d17a215e7
Nimrat Sandhu <nimratsandhu@Nimrats-MacBook-Pro.local>
2025-06-06 23:14:13 -0400

    Migration 1: adding fields to event table

--- app/models.py
+++ app/models.py
@@ -3,3 +3,6 @@ from . import db
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), nullable=False)
+    description = db.Column(db.String(500))
+    start_time = db.Column(db.DateTime)
+    end_time = db.Column(db.DateTime)
