bf1893e83e16d3fd09eaf8177f558c1078e3c161
Nimrat Sandhu <nimratsandhu@Nimrats-MacBook-Pro.local>
2025-06-06 23:14:13 -0400

    Migration 1: adding fields to event table

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import os
 
 DB_URL = os.getenv("DB_URL")
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
@@ -12,6 +14,7 @@ def create_app():
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
+    migrate.init_app(app, db)
 
     from .routes import main
     app.register_blueprint(main)
